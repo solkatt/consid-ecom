b0b736ff2570be3f93aeb258ffa0d3db932ee432
solkatt <viktoralexanderodman@gmail.com>
2022-12-14 13:46:12 +0100

    Update styling

--- components/products/ProductCard.js
+++ components/products/ProductCard.js
@@ -1,13 +1,28 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 export default function ProductCard({ product }) {
-
-  return (
-    <div key={product.id} style={{ maxWidth: '400px' }}>
-      <Link href={`/product/${product.id}`}>
-        <img src={product.mainImage.url} alt="" style={{ width: '100%' }} />
-        <h2>{product.name}</h2>
-      </Link>
-      <p>{product.price}</p>
-    </div>)
+	return (
+		<div
+			key={product.id}
+			style={{
+				maxWidth: '400px',
+				background: 'white',
+				boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px;',
+				margin: '20px',
+				borderRadius: '0 0 30px 30px',
+			}}
+		>
+			<Link href={`/product/${product.id}`}>
+				<img
+					src={product.mainImage.url}
+					alt=''
+					style={{ width: '100%', borderRadius: '20px 20px 0 0' }}
+				/>
+				<div style={{ padding: '0.2rem 1rem' }}>
+					<h2>{product.name}</h2>
+					<p style={{ marginBottom: '1rem' }}>{product.price}:-</p>
+				</div>
+			</Link>
+		</div>
+	)
 }
